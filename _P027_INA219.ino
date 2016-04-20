5024164e790fff40dcd32a1d1811f3cb32bf3ce9
ogollemon <ogollemon@users.noreply.github.com>
2016-04-20 03:08:28 +0200

    Update _P027_INA219.ino

--- _P027_INA219.ino
+++ _P027_INA219.ino
@@ -157,7 +157,7 @@ boolean Plugin_027(byte function, struct EventStruct *event, String& string)
         String plugin1 = WebServer.arg("plugin_026_value");
         Settings.TaskDevicePluginConfig[event->TaskIndex][0] = plugin1.toInt();
 //      String plugin1 = WebServer.arg("plugin_026_adr");
-//        Settings.TaskDevicePluginConfig[event->TaskIndex][0] = plugin2.toInt();
+//        Settings.TaskDevicePluginConfig[event->TaskIndex][1] = plugin2.toInt();
         success = true;
         break;
       }
