48287911bac44de9e9a66d4808aa6e4c9b687972
ogollemon <ogollemon@users.noreply.github.com>
2016-04-24 17:14:24 +0200

    Update _P027_INA219.ino
    Plugin_027_init was not working

--- _P027_INA219.ino
+++ _P027_INA219.ino
@@ -8,7 +8,7 @@
 #define PLUGIN_VALUENAME1_027 "Voltage"
 
 uint8_t Plugin_027_INA219_address = 0x40; // 0x40 /0x41 A0/0x44 A1/0x45 A0 and A1
-boolean Plugin_027_init = false;
+//boolean Plugin_027_init = false;
 #define INA219_READ                            (0x01)
 #define INA219_REG_CONFIG                      (0x00)
 #define INA219_CONFIG_RESET                    (0x8000)  // Reset Bit
@@ -157,14 +157,14 @@ boolean Plugin_027(byte function, struct EventStruct *event, String& string)
         Settings.TaskDevicePluginConfig[event->TaskIndex][0] = plugin1.toInt();
       String plugin2 = WebServer.arg("plugin_027_adr");
         Settings.TaskDevicePluginConfig[event->TaskIndex][1] = plugin2.toInt();
-        Plugin_027_init = false; // Force device setup next time??
+   //     Plugin_027_init = false; // Force device setup next time??
         success = true;
         break;
       }
 
     case PLUGIN_INIT:
       {
-        boolean Plugin_027_init = true; // Force device setup??
+    //    boolean Plugin_027_init = true; // Force device setup??
         Plugin_027_INA219_address = Settings.TaskDevicePluginConfig[event->TaskIndex][1];
         Plugin_027_begin();
         success = true;
